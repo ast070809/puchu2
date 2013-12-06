d0b40ed9d1442848aad2618202dad9a3afd35c0f
ast0708 <ast0708@gmail.com>
2013-12-06 15:05:14 +0530

    user model

---
+++ puchu/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Puchu::Application.config.secret_key_base = '7a70c4550593b7f0bad69578a9323bfb1a7c75d45f222bc601338116fa758a4ff87931362349c27837eb30d98138bb5ab1db0a46614bab4b7b5098f687a988d1'
